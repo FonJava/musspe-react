e3ee7f23494b6504f5dea1c2611b9329845d63af
Anthony Guerra <formalanthonyefraim@gmail.com>
2025-07-06 15:42:03 -0300

    estrutura inicial

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header/Header";
+import Sidebar from "./components/Sidebar/Sidebar";
+import Footer from "./components/Footer/Footer";
+
+import "./assets/css/estilo.css";
+
+import Home from "./pages/Home/Home";
+import Noticias from "./pages/Noticias/Noticias";
+import Acervo from "./pages/Acervo/Acervo";
+import Colaboradores from "./pages/Colaboradores/Colaboradores";
+import Visita from "./pages/Visita/Visita";
+import Jogos from "./pages/Jogos/Jogos";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <BrowserRouter>
+      <Header />
+      <Sidebar />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/noticias" element={<Noticias />} />
+          <Route path="/acervo" element={<Acervo />} />
+          <Route path="/jogos" element={<Jogos />} />
+          <Route path="/colaboradores" element={<Colaboradores />} />
+          <Route path="/visita" element={<Visita />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </main>
+      <Footer />
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
