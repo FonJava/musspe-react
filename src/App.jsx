a6a4e62f2f435936443fe0f66be24b024265130b
Anthony Guerra <formalanthonyefraim@gmail.com>
2025-07-10 16:28:32 -0300

    barra de navegação (desktop)

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import Header from "./components/Header/Header";
 /* import Sidebar from "./components/Sidebar/Sidebar";
 import Footer from "./components/Footer/Footer"; */
@@ -11,25 +12,47 @@ import Visita from "./pages/Visita/Visita";
 import Jogos from "./pages/Jogos/Jogos"; */
 import NotFound from "./pages/NotFound/NotFound";
 
-function App() {
+function AppWrapper() {
+  const location = useLocation();
+  const [activePage, setActivePage] = useState("home");
+
+  useEffect(() => {
+    const pathToPage = {
+      "/": "home",
+      "/noticias": "noticias",
+      "/acervo": "acervo",
+      "/jogos": "jogos",
+      "/colaboradores": "colaboradores",
+      "/visita": "visita",
+    };
+    const page = pathToPage[location.pathname] || "notfound";
+    setActivePage(page);
+  }, [location.pathname]);
+
   return (
-    <BrowserRouter>
-      <Header />
-      {/* <Sidebar /> */}
+    <>
+      <Header activePage={activePage} setActivePage={setActivePage} />
       <main>
         <Routes>
           <Route path="/" element={<Home />} />
-          {/* <Route path="/noticias" element={<Noticias />} />
+          {/* 
+          <Route path="/noticias" element={<Noticias />} />
           <Route path="/acervo" element={<Acervo />} />
           <Route path="/jogos" element={<Jogos />} />
           <Route path="/colaboradores" element={<Colaboradores />} />
-          <Route path="/visita" element={<Visita />} />*/}
+          <Route path="/visita" element={<Visita />} />
+          */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </main>
-      {/* <Footer /> */}
-    </BrowserRouter>
+    </>
   );
 }
 
-export default App;
+export default function App() {
+  return (
+    <BrowserRouter>
+      <AppWrapper />
+    </BrowserRouter>
+  );
+}
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import logo from "../../../public/imagens/logo-cabecalho.png";
 
-export default function Header() {
+export default function Header({ activePage, setActivePage }) {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
   const toggleMobileMenu = () => {
@@ -10,81 +10,109 @@ export default function Header() {
   };
 
   return (
-    <header className="bg-white shadow-md relative z-50">
-      <div className="container mx-auto flex items-center justify-between p-4">
+    <header className="bg-brand-dark shadow-md relative z-50">
+      <div
+        className="w-full mx-auto flex items-center justify-between p-4 max-w-2xl md:max-w-3xl
+       lg:max-w-4xl xl:max-w-5xl"
+      >
         {/* Logo */}
         <Link to="/" className="flex items-center">
           <img src={logo} alt="Logo Musspe" className="h-10 w-auto mr-2" />
-          <h1 className="text-2xl font-barlow-black text-brand-dark">MUSSPE</h1>
+          <h1 className="text-2xl font-barlow-black text-brand-bege">MUSSPE</h1>
         </Link>
 
         {/* Botão do menu mobile */}
         <button
-          className="md:hidden text-gray-800 focus:outline-none"
+          className="md:hidden w-6 h-6 bg-no-repeat bg-center focus:outline-none 
+          bg-[url('data:image/svg+xml;charset=utf8,%3Csvg%20viewBox=%270%200%2030%2030%27%20xmlns=%27http://www.w3.org/2000/svg%27%3E%3Cpath%20stroke=%27rgba(255,220,210,1)%27%20stroke-width=%272%27%20stroke-linecap=%27round%27%20stroke-miterlimit=%2710%27%20d=%27M4%207h22M4%2015h22M4%2023h22%27/%3E%3C/svg%3E')]
+          border border-brand-laranja rounded px-7 py-5 bg-[length:1.8rem_1.8rem]"
           onClick={toggleMobileMenu}
-        >
-          <svg
-            className="w-6 h-6"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M4 6h16M4 12h16M4 18h16"
-            />
-          </svg>
-        </button>
+        ></button>
 
         {/* Navegação desktop */}
         <nav className="hidden md:block">
-          <ul className="flex space-x-6">
+          <ul className="flex space-x-5 text-md text-white font-barlow-semibold">
             <li>
               <Link
                 to="/"
-                className="text-gray-600 hover:text-gray-900 transition-colors
-                 duration-200"
+                className="relative"
+                onClick={() => setActivePage("home")}
               >
-                Início
+                <span className="relative z-10">Início</span>
+                <div
+                  className={`absolute top-3/4 left-0 right-0 h-1 bg-brand-tangerina rounded-full transition-all duration-300 ease-out ${
+                    activePage === "home" ? "scale-x-100 opacity-100" : "scale-x-0 opacity-0"
+                  }`}
+                />
               </Link>
             </li>
             <li>
               <Link
                 to="/noticias"
-                className="text-gray-600 hover:text-gray-900 transition-colors
-                 duration-200"
+                className="relative"
+                onClick={() => setActivePage("noticias")}
               >
-                Notícias
+                <span className="relative z-10">Notícias</span>
+                <div
+                  className={`absolute top-3/4 left-0 right-0 h-1 bg-brand-tangerina rounded-full transition-all duration-300 ease-out ${
+                    activePage === "noticias" ? "scale-x-100 opacity-100" : "scale-x-0 opacity-0"
+                  }`}
+                />
               </Link>
             </li>
             <li>
               <Link
                 to="/acervo"
-                className="text-gray-600 hover:text-gray-900 transition-colors
-                 duration-200"
+                className="relative"
+                onClick={() => setActivePage("acervo")}
               >
-                Acervo
+                <span className="relative z-10">Acervo</span>
+                <div
+                  className={`absolute top-3/4 left-0 right-0 h-1 bg-brand-tangerina rounded-full transition-all duration-300 ease-out ${
+                    activePage === "acervo" ? "scale-x-100 opacity-100" : "scale-x-0 opacity-0"
+                  }`}
+                />
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/jogos"
+                className="relative"
+                onClick={() => setActivePage("jogos")}
+              >
+                <span className="relative z-10">Jogos</span>
+                <div
+                  className={`absolute top-3/4 left-0 right-0 h-1 bg-brand-tangerina rounded-full transition-all duration-300 ease-out ${
+                    activePage === "jogos" ? "scale-x-100 opacity-100" : "scale-x-0 opacity-0"
+                  }`}
+                />
               </Link>
             </li>
             <li>
               <Link
                 to="/colaboradores"
-                className="text-gray-600 hover:text-gray-900 transition-colors
-                 duration-200"
+                className="relative"
+                onClick={() => setActivePage("colaboradores")}
               >
-                Colaboradores
+                <span className="relative z-10">Colaboradores</span>
+                <div
+                  className={`absolute top-3/4 left-0 right-0 h-1 bg-brand-tangerina rounded-full transition-all duration-300 ease-out ${
+                    activePage === "colaboradores" ? "scale-x-100 opacity-100" : "scale-x-0 opacity-0"
+                  }`}
+                />
               </Link>
             </li>
             <li>
               <Link
                 to="/visita"
-                className="text-gray-600 hover:text-gray-900 transition-colors
-                 duration-200"
+                className={
+                  activePage === "visita"
+                    ? "text-white bg-brand-laranja rounded-full px-1.5 py-1 transition-colors duration-200 ease-out focus:outline-none border-2 border-transparent"
+                    : "text-brand-laranja border-2 border-brand-laranja rounded-full px-1.5 py-1 transition-colors duration-200 ease-out focus:outline-none"
+                }
+                onClick={() => setActivePage("visita")}
               >
-                Visita
+                Agende uma Visita
               </Link>
             </li>
           </ul>
