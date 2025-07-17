0a3e270611146f30b87a8a9b9af081dc53124c96
Anthony Guerra <formalanthonyefraim@gmail.com>
2025-07-17 14:15:09 -0300

    teste3

--- src/components/Barra-visita.jsx
+++ src/components/Barra-visita.jsx
@@ -1,6 +1,6 @@
 /* Barra-visita */
 import React from "react";
-import Botao from "./Botaoo";
+import Botao from "./Botao";
 import { Link } from "react-router-dom";
 
 export default function BarraVisita() {
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import Botao from "../../components/Botaoo";
+import Botao from "../../components/Botao";
 import imgesq from "/imagens/sobre-nos-esquerda.png";
 import imgdir from "/imagens/sobre-nos-direita.png";
 import sobrenos from "/imagens/sobre-nos-imagem.png";
 import sobrenosicon from "/imagens/sobre-nos-icon.png";
-import BarraVisita from "../../components/Barra-visitaa";
+import BarraVisita from "../../components/Barra-visita";
 import Apresentacao from "../../components/Apresentacao";
 import mateuszinho from "/imagens/mateuzinho.png";
 
